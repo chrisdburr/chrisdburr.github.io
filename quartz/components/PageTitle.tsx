3990ece21334c60af1fadeb57d8796e59bd39529
Christopher Burr <chrisdburr@gmail.com>
2024-02-08 07:26:00 +0000

    site preview build

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -5,27 +5,28 @@ function PageTitle({ fileData, cfg, displayClass }: QuartzComponentProps) {
   const title = cfg?.pageTitle ?? "Untitled Quartz"
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <div class={`page-title-container ${displayClass ?? ""}`}>
-      <img
-        src="https://github.com/chrisdburr.png"
-        alt="Profile Picture"
-        class="profile-pic"
-      />
+    <div id="page-title-container">
+      <img src="https://github.com/chrisdburr.png" alt="Profile Picture" class="profile-pic" />
       <h1 class="page-title">
-        <a href={baseDir}>{title}</a>
+        <a href="{baseDir}">{title}</a>
       </h1>
     </div>
   )
 }
 
 PageTitle.css = `
-.page-title-container {
+#page-title-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   text-align: center;
+  width: 100%
 }
 
 .profile-pic {
-  width: 70%; /* Adjust as necessary */
-  height: 70%; /* Adjust as necessary */
+  width: 50%; /* Adjust as necessary */
+  height: auto; /* Adjust for aspect ratio */
   border-radius: 50%;
   object-fit: cover;
   margin-bottom: 10px; /* Space between image and title */
